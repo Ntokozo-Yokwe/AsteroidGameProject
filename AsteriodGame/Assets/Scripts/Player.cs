2cb38fdc17090ccd0ff009e76ff9bb9b2022ebc3
Ntokozo-Yokwe <64004430+Ntokozo-Yokwe@users.noreply.github.com>
2022-02-16 19:32:56 +0200

    Asteroid game full upload

---
+++ AsteriodGame/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+
+
+    public float speed;
+    public float rotationOffset;
+    public Bullet bulletPrefab;
+
+    public float respawnDelay = 3.0f;
+    public float respawnInvulnerability = 3.0f;
+
+    private void OnEnable()
+    {
+        // Turn off collisions for a few seconds after spawning to ensure the
+        // player has enough time to safely move away from asteroids
+        this.gameObject.layer = LayerMask.NameToLayer("Ignore Collisions");
+        Invoke(nameof(TurnOnCollisions), this.respawnInvulnerability);
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + rotationOffset));
+
+        Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPos.z = 0;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
+    }
+
+    private void TurnOnCollisions()
+    {
+        this.gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Asteroid")
+        {
+
+            this.gameObject.SetActive(false);
+
+            FindObjectOfType<GameManager>().PlayerDeath(this);
+        }
+    }
+
+}
---
+++ AsteriodGame/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+}
